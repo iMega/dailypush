7de05d7935888f5580abe30d96c1207ccbf10818
Dmitry <irvis@imega.ru>
2016-09-13 16:45:26 +0300

    Random range 1..5 commits per day

--- daily.sh
+++ daily.sh
@@ -1,5 +1,11 @@
-cd ~/dailypush/
-echo "." >> README.md
-git add *
-git commit -m 'New changes'
-git push origin master
+#!/usr/bin/env bash
+
+# Random range 1..5 commits per day
+REPEAT=$(( ( RANDOM % 5 )  + 1 ))
+until [ $REPEAT -lt 1 ]; do
+    echo "." >> README.md
+    git add README.md
+    git commit -m 'New changes'
+    git push origin master
+    let REPEAT-=1
+done
